62725c9bd50392ba80e9f7f1b4ba66ee5557e0af
alov <albert.alov@collabio.team>
2019-04-02 15:11:10 +0300

    right coding-style

--- exercise12.js
+++ exercise12.js
@@ -1,35 +1,37 @@
 /**Задача №12.1 */
-document.getElementById('exercise12.1').onclick=function(){
-    let rectangle = {
-        a: 8,
-        b: 7
-    };
+document.getElementById('exercise12.1').onclick=function() {
+  let rectangle = {
+    a: 8,
+    b: 7
+  };
 
-    function changeSides(k){
-        if(k < 0){
-            alert('k не может быть отрицательным')
-        } else {
-           rectangle.a *= k;
-           rectangle.b *= k;
-           alert('Теперь стороны равны: '+ rectangle.a + ' и '+ rectangle.b);
-        }
+  function changeSides(k) {
+    if (k < 0) {
+      alert('k не может быть отрицательным')
+    } else {
+      rectangle.a *= k;
+      rectangle.b *= k;
+      alert('Теперь стороны равны: '+ rectangle.a + ' и '+ rectangle.b);
     }
+  }
 
-    alert('Сейчас стороны равны: ' + rectangle.a + ' и '+ rectangle.b);
-    let K = prompt('Введите число K:', '');
-    changeSides(K);
+  alert('Сейчас стороны равны: ' + rectangle.a + ' и '+ rectangle.b);
+  let K = prompt('Введите число K:', '');
+  changeSides(K);
 }
 
 /**Задача №12.2 */
-document.getElementById('exercise12.2').onclick=function(){
-    function sum(){
-        let arr = [];
-        arr.length = 8;
+document.getElementById('exercise12.2').onclick=function() {
+  function sum(){
+    let arr = [];
+    arr.length = 8;
 
-        for(let i = 0; i < arr.length; i++){
-            arr[i] = prompt('Введите, ' + i + '-ый элемент из массива длинной 8', '');
-        }
-        return arr;
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = prompt('Введите, ' + i + '-ый элемент из массива длинной 8', '');
     }
-    alert('Наш массив выглядит так: ' + sum());
+
+    return arr;
+  }
+
+  alert('Наш массив выглядит так: ' + sum());
 }
--- exercise13.js
+++ exercise13.js
@@ -1,24 +1,24 @@
 /**Задача №13 */
 document.getElementById('exercise13').onclick=function() {
-    //let dnk = 'aaaaaeeeefffAaAAAa';        
+  //let dnk = 'aaaaaeeeefffAaAAAa';        
     
-    function algorithmDNK(arr) {
-      let newArr = '';
-      let count = 1;
-      let i = 0;
+  function algorithmDNK(arr) {
+    let newArr = '';
+    let count = 1;
+    let i = 0;
 
-      while (i < arr.length) {
-        if (arr[i] == arr[i+1]){
-          count += 1;
-        } else {
-          newArr = newArr + arr[i] + count;
-          count = 1;
-        }
-          i++;
-        }
-        return newArr;
-    }
+    while (i < arr.length) {
+      if (arr[i] == arr[i+1]){
+        count += 1;
+      } else {
+        newArr = newArr + arr[i] + count;
+        count = 1;
+      }
+        i++;
+      }
+      return newArr;
+  }
 
-    let dnk = prompt('Введите цепочку DNK: ', '');
-    alert( algorithmDNK(dnk) );
+  let dnk = prompt('Введите цепочку DNK: ', '');
+  alert( algorithmDNK(dnk) );
 }
